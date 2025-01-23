39d8a49a5952680fd48445652ec063c97bc697c4
cuphalffull <lissetteschettini@gmail.com>
2025-01-22 19:32:30 -0800

    added contact js and more code to other files

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,8 @@
 (function (){
-"use strict"
+"use strict";
 
 
-    document.querySelector ("#testButton").addEventListener("click", handleClick);
 
-function handleClick() {
-console.log("Thank you for clicking.");
-}
 
-}());
+
+})();
