33d510ff460d309757d4bb215d50566113e5e2bb
cuphalffull <lissetteschettini@gmail.com>
2025-02-17 21:50:42 -0800

    Added MySQL and it is working

--- utils/utils.js
+++ utils/utils.js
@@ -1,9 +1,8 @@
 import nodemailer from "nodemailer";
 
-async function sendMessage(sub, txt){
-    let transporter = nodemailer.createTransport(
-        {
-            host: process.env.MAIL_HOST,
+export async function sendMessage(sub, txt) {
+  let transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
     secure: process.env.MAIL_SECURE,
     auth: {
@@ -11,20 +10,21 @@ async function sendMessage(sub, txt){
       pass: process.env.MAIL_PASSWORD,
     },
     requireTLS: process.env.MAIL_TLS,
-        }
-    )
-let message ={
+  });
+
+  let message = {
     from: process.env.MESSAGE_FROM,
     to: process.env.MESSAGE_TO,
     subject: sub,
     text: txt,
-};
-await transporter
+  };
+
+  await transporter
     .sendMail(message)
     .then(() => {
       console.log("Message sent");
     })
     .catch((err) => {
       console.log("Message not sent - " + err);
-  });
+    });
 }
